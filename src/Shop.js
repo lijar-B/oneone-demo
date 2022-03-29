2acb8a1d73c0eea6b1c449161f1d01607a5e3663
lijar <beljar1214@gmail.com>
2022-03-29 19:36:14 +0630

    dd

---
+++ src/Shop.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const shop = () => {
+  document.title = `NaMa-WanWan`
+  return (
+    <div>
+      <img className="shop-img" src="./img/42 (OneOne)/1 (1).jpg" alt="" />
+      <img className="shop-img" src="./img/42 (OneOne)/1 (2).jpg" alt="" />
+      <img className="shop-img" src="./img/42 (OneOne)/1 (3).jpg" alt="" />
+      <img className="shop-img" src="./img/42 (OneOne)/1 (4).jpg" alt="" />
+      <img className="shop-img" src="./img/42 (OneOne)/1 (5).jpg" alt="" />
+      <img className="shop-img" src="./img/42 (OneOne)/1 (6).jpg" alt="" />
+    </div>
+  )
+}
+
+export default shop
